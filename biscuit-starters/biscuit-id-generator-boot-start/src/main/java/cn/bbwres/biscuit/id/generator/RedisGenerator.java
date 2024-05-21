696136c7672636e808985f04bd86681a2d52f9de
jimodebeiju123 <657966747@qq.com>
2024-05-21 10:52:48 +0800

    增加基于redis 的号段id获取

--- biscuit-starters/biscuit-id-generator-boot-start/src/main/java/cn/bbwres/biscuit/id/generator/RedisGenerator.java
+++ biscuit-starters/biscuit-id-generator-boot-start/src/main/java/cn/bbwres/biscuit/id/generator/RedisGenerator.java
@@ -1,10 +1,14 @@
 package cn.bbwres.biscuit.id.generator;
 
+import cn.bbwres.biscuit.exception.SystemRuntimeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * 基于redis的有序的id生成
@@ -32,15 +36,35 @@ public class RedisGenerator {
 
 
     /**
-     * 获取id
+     * 获取指定数量的id, id步进为1
      *
-     * @param key key
-     * @param prefix 前缀
-     * @param noPrefixSize 不包含前缀的长度
-     * @param stemNum 步进
+     * @param prefix
      * @return
      */
-    public String nextId(String key,String prefix, Integer noPrefixSize, Long stemNum) {
+    public List<String> nextIds(String prefix, Long num) {
+        Long maxId = redisTemplate.opsForValue().increment(REDIS_ID_KEY + prefix, num);
+        if (Objects.isNull(maxId)) {
+            log.warn("从redis中获取id失败！");
+            throw new SystemRuntimeException("id_fail");
+        }
+        long minId = maxId - num;
+        List<String> ids = new ArrayList<>(16);
+        for (long i = minId; i <= maxId; i++) {
+            ids.add(prefix + i);
+        }
+        return ids;
+    }
+
+    /**
+     * 获取id
+     *
+     * @param key          key
+     * @param prefix       前缀
+     * @param noPrefixSize 不包含前缀的长度
+     * @param stemNum      步进
+     * @return
+     */
+    public String nextId(String key, String prefix, Integer noPrefixSize, Long stemNum) {
         Long num = redisTemplate.opsForValue().increment(REDIS_ID_KEY + key, stemNum == null ? 1L : stemNum);
         String strNum = num + "";
         int size = noPrefixSize == null ? 10 : noPrefixSize;
