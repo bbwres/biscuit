d419952eaab5224e49ea092d68af7395dc45c262
jimodebeiju123 <657966747@qq.com>
2024-06-20 09:50:03 +0800

    1.优化i18n配置

--- biscuit-starters/biscuit-common-boot-starter/src/main/java/cn/bbwres/biscuit/exception/constants/GlobalErrorCodeConstants.java
+++ biscuit-starters/biscuit-common-boot-starter/src/main/java/cn/bbwres/biscuit/exception/constants/GlobalErrorCodeConstants.java
@@ -36,19 +36,20 @@ public interface GlobalErrorCodeConstants {
      */
     ErrorCode GLOBAL_HTTP_CODE_PREFIX = new ErrorCode("100000", "http状态码前缀");
 
-    ErrorCode BAD_REQUEST = new ErrorCode("100000400", "请求参数不正确");
-    ErrorCode UNAUTHORIZED = new ErrorCode("100000401", "账号未登录");
-    ErrorCode FORBIDDEN = new ErrorCode("100000403", "没有该操作权限");
-    ErrorCode NOT_FOUND = new ErrorCode("100000404", "请求未找到");
-    ErrorCode METHOD_NOT_ALLOWED = new ErrorCode("100000405", "请求方法不正确");
-    ErrorCode LOCKED = new ErrorCode("100000423", "请求失败，请稍后重试");
-    ErrorCode TOO_MANY_REQUESTS = new ErrorCode("100000429", "请求过于频繁，请稍后重试");
-    ErrorCode INTERNAL_SERVER_ERROR = new ErrorCode("100000500", "系统异常");
+    ErrorCode BAD_REQUEST = new ErrorCode("100000400", "global.bad_request");
+    ErrorCode UNAUTHORIZED = new ErrorCode("100000401", "global.unauthorized");
+    ErrorCode FORBIDDEN = new ErrorCode("100000403", "global.forbidden");
+    ErrorCode NOT_FOUND = new ErrorCode("100000404", "global.not_found");
+    ErrorCode METHOD_NOT_ALLOWED = new ErrorCode("100000405", "global.method_not_allowed");
+    ErrorCode LOCKED = new ErrorCode("100000423", "global.locked");
+    ErrorCode TOO_MANY_REQUESTS = new ErrorCode("100000429", "global.too_many_requests");
+    ErrorCode INTERNAL_SERVER_ERROR = new ErrorCode("100000500", "global.internal_server_error");
 
 
     //############################## 登录相关错误 ##########################################
 
-    ErrorCode INVALID_CAPTCHA = new ErrorCode("101000100", "验证码错误");
+    ErrorCode INVALID_CAPTCHA = new ErrorCode("101000100", "global.invalid_captcha");
+    ErrorCode INVALID_TOKEN = new ErrorCode("101000101", "global.invalid_token");
 
     // ############################### 业务错误码 ##############################
 
@@ -58,5 +59,5 @@ public interface GlobalErrorCodeConstants {
      * 000 业务
      * 000 异常明细
      */
-    ErrorCode BUSINESS_ERROR = new ErrorCode("200000001", "业务处理失败");
+    ErrorCode BUSINESS_ERROR = new ErrorCode("200000001", "global.business_error");
 }
